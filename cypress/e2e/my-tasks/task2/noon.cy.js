024f2fe5bf665d638a9c218d32961fc9011241b1
emad-almajdalawi <e.almajdalawi@gamil.com>
2022-08-21 16:27:17 +0300

    fix: save the product's name

--- cypress/e2e/my-tasks/task2/noon.cy.js
+++ cypress/e2e/my-tasks/task2/noon.cy.js
@@ -8,29 +8,30 @@ describe('cart', () => {
     it('select one product and add it to the cart', () => {
         // search for a product
         cy.get('#searchBar').type('iphone {enter}')
-        cy.wait(2000)
 
-        // select the first product
-        cy.get('#productBox-N50840187A > .sc-9e9305b-0 > .sc-2f7ba0e9-0 > .sc-2f7ba0e9-10 > .sc-2f7ba0e9-12 > [width="0"] > :nth-child(1) > :nth-child(1)')
+        // select the first product then open its page
+        cy.wait(4000)
+        cy.get('#productBox-N50840187A > .sc-9e9305b-0').click()
+
+        // save the product's name
+        cy.get('.sc-8a26e3fa-12')
             .then(message => {
                 let product_name = message.text()
                 console.log(product_name)
-
-                // open product page
-                cy.get('.sc-5e739f1b-0.gEERDr.wrapper.productContainer').contains(`${product_name}`).click()
                 cy.wait(2000)
 
                 // add product to the cart
                 cy.get('.sc-8a26e3fa-14 > .sc-956a1cfb-1 > .cart-button > .loaderCtr')
                     .click()
-                cy.wait(1000)
+                cy.wait(2000)
                 cy.get('#continue-shopping-btn > .sc-af60a083-1')
                     .click()
-                cy.wait(1000)
+                cy.wait(2000)
 
                 // open cart page
                 cy.get('.cartLink')
                     .click()
+                cy.wait(2000)
                 cy.get(' div.sc-ce9d6f0f-7.cRyAUI > h1')
                     .should('contain', `${product_name}`)
             })
