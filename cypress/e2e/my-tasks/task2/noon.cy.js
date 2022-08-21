36cb7f2bfd48ea0d18a63886f310bd2216dc4a56
emad-almajdalawi <e.almajdalawi@gamil.com>
2022-08-21 17:14:04 +0300

    fix: variables and description

--- cypress/e2e/my-tasks/task2/noon.cy.js
+++ cypress/e2e/my-tasks/task2/noon.cy.js
@@ -1,30 +1,41 @@
 /// <reference types="cypress" />
 
 describe('cart', () => {
-    it('visit', () => {
+    const firstProduct = '#productBox-N50840187A > .sc-9e9305b-0'
+    const productNmaeFromProductPage = '.sc-8a26e3fa-12'
+    const addToCartBtn = '.sc-8a26e3fa-14 > .sc-956a1cfb-1 > .cart-button > .loaderCtr'
+    const continueShopingBtn = '#continue-shopping-btn > .sc-af60a083-1'
+    const productNameFromCartPage = 'div.sc-ce9d6f0f-7.cRyAUI > h1'
+    const removeBtn = '.sc-ce9d6f0f-24'
+    const mainBodySecstion = '.sc-64a8c7b2-0'
+
+    it('should visit the home page', () => {
         cy.visit('https://www.noon.com/uae-en/')
     })
 
-    it('select one product and add it to the cart', () => {
+    it('shuold search for a product then click on it', () => {
         // search for a product (iphone)
         cy.get('#searchBar').type('iphone {enter}')
+        cy.wait(4000)
 
         // select the first product then open its page
-        cy.wait(4000)
-        cy.get('#productBox-N50840187A > .sc-9e9305b-0').click()
+        cy.get(firstProduct).click()
 
+    })
+
+    it('should add the product to cart', () => {
         // save the product's name
-        cy.get('.sc-8a26e3fa-12')
+        cy.get(productNmaeFromProductPage)
             .then(message => {
-                let product_name = message.text()
-                console.log(product_name)
+                let productName = message.text()
+                console.log(productName)
                 cy.wait(2000)
 
                 // add product to the cart
-                cy.get('.sc-8a26e3fa-14 > .sc-956a1cfb-1 > .cart-button > .loaderCtr')
+                cy.get(addToCartBtn)
                     .click()
                 cy.wait(2000)
-                cy.get('#continue-shopping-btn > .sc-af60a083-1')
+                cy.get(continueShopingBtn)
                     .click()
                 cy.wait(2000)
 
@@ -32,12 +43,14 @@ describe('cart', () => {
                 cy.get('.cartLink')
                     .click()
                 cy.wait(2000)
-                cy.get(' div.sc-ce9d6f0f-7.cRyAUI > h1')
-                    .should('contain', `${product_name}`)
+                cy.get(productNameFromCartPage)
+                    .should('contain', `${productName}`)
             })
+    })
 
+    it('should remove the product from cart', () => {
         // remove the product from the cart
-        cy.get('.sc-ce9d6f0f-24')
+        cy.get(removeBtn)
             .click()
         cy.wait(2000)
 
@@ -45,7 +58,7 @@ describe('cart', () => {
         cy.get('.cartLink')
             .click()
         cy.wait(2000)
-        cy.get('.sc-64a8c7b2-0')
+        cy.get(mainBodySecstion)
             .should('contain', 'Your shopping cart looks empty')
     })
 })
