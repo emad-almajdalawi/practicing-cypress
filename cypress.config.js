95099d64979c46a9dba6f6f9738c73c4b9258da8
emad-almajdalawi <e.almajdalawi@gamil.com>
2022-08-22 16:36:06 +0300

    config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        baseUrl: 'https://www.noon.com/uae-en/',
+        searchBar: '#searchBar',
+        firstProduct: '#productBox-N50840187A > .sc-9e9305b-0',
+        productNmaeFromProductPage: '.sc-8a26e3fa-12',
+        addToCartBtn: '.sc-8a26e3fa-14 > .sc-956a1cfb-1 > .cart-button > .loaderCtr',
+        continueShopingBtn: '#continue-shopping-btn > .sc-af60a083-1',
+        cartBtn: '.cartLink',
+        productNameFromCartPage: 'div.sc-ce9d6f0f-7.cRyAUI > h1',
+        removeBtn: '.sc-ce9d6f0f-24',
+        mainBodySecstion: '.sc-64a8c7b2-0',
+
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
 });
--- cypress/e2e/my-tasks/task2/noon.cy.js
+++ cypress/e2e/my-tasks/task2/noon.cy.js
@@ -1,30 +1,19 @@
 /// <reference types="cypress" />
 
 describe('cart', () => {
-    const homePage = 'https://www.noon.com/uae-en/'
-    const searchBar = '#searchBar'
-    const firstProduct = '#productBox-N50840187A > .sc-9e9305b-0'
-    const productNmaeFromProductPage = '.sc-8a26e3fa-12'
-    const addToCartBtn = '.sc-8a26e3fa-14 > .sc-956a1cfb-1 > .cart-button > .loaderCtr'
-    const continueShopingBtn = '#continue-shopping-btn > .sc-af60a083-1'
-    const cartBtn = '.cartLink'
-    const productNameFromCartPage = 'div.sc-ce9d6f0f-7.cRyAUI > h1'
-    const removeBtn = '.sc-ce9d6f0f-24'
-    const mainBodySecstion = '.sc-64a8c7b2-0'
     var productName = ''
 
-
     it('Should visit the home page', () => {
-        cy.visit(homePage)
+        cy.visit(Cypress.config('baseUrl'))
     })
 
     it('Should search for a product then click on the first result', () => {
-        cy.get(searchBar).type('iphone {enter}')
-        cy.get(firstProduct).click()
+        cy.get(Cypress.config('searchBar')).type('iphone {enter}')
+        cy.get(Cypress.config('firstProduct')).click()
     })
 
     it("Should save the product's name", () => {
-        cy.get(productNmaeFromProductPage)
+        cy.get(Cypress.config('productNmaeFromProductPage'))
             .then(message => {
                 productName = message.text()
 
@@ -32,24 +21,24 @@ describe('cart', () => {
     })
 
     it('Should add the product to cart', () => {
-        cy.get(addToCartBtn)
+        cy.get(Cypress.config('addToCartBtn'))
             .click()
-        cy.get(continueShopingBtn)
+        cy.get(Cypress.config('continueShopingBtn'))
             .click()
 
-        cy.get(cartBtn)
+        cy.get(Cypress.config('cartBtn'))
             .click()
-        cy.get(productNameFromCartPage)
+        cy.get(Cypress.config('productNameFromCartPage'))
             .should('contain', `${productName}`)
     })
 
     it('Should remove the product from cart', () => {
-        cy.get(removeBtn)
+        cy.get(Cypress.config('removeBtn'))
             .click()
 
-        cy.get(cartBtn)
+        cy.get(Cypress.config('cartBtn'))
             .click()
-        cy.get(mainBodySecstion)
+        cy.get(Cypress.config('mainBodySecstion'))
             .should('contain', 'Your shopping cart looks empty')
     })
 })
