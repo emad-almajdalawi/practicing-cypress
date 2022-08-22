7fe5f96d5094d4f5657882a752fca5e1c86ec150
emad-almajdalawi <e.almajdalawi@gamil.com>
2022-08-22 17:33:10 +0300

    Fix: inside for loop

--- cypress/e2e/my-tasks/task2/noon.cy.js
+++ cypress/e2e/my-tasks/task2/noon.cy.js
@@ -1,94 +1,55 @@
 /// <reference types="cypress" />
 
 describe('cart', () => {
-    context('Desktop Resolution', () => {
-        var productName = ''
+    let views = ['Desktop Resolution', 'Mobile Resolution']
+    views.forEach(view => {
+        context('Desktop Resolution', () => {
+            var productName = ''
 
-        it('Should visit the home page', () => {
-            cy.visit(Cypress.config('baseUrl'))
-        })
-
-        it('Should search for a product then click on the first result', () => {
-            cy.get(Cypress.config('searchBar')).type('iphone {enter}')
-            cy.get(Cypress.config('firstProduct')).click()
-        })
-
-        it("Should save the product's name", () => {
-            cy.get(Cypress.config('productNmaeFromProductPage'))
-                .then(message => {
-                    productName = message.text()
-
+            if (view == 'Mobile Resolution') {
+                beforeEach(() => {
+                    cy.viewport('iphone-x')
                 })
-        })
+            }
+            it('Should visit the home page', () => {
+                cy.visit(Cypress.config('baseUrl'))
+            })
 
-        it('Should add the product to cart', () => {
-            cy.get(Cypress.config('addToCartBtn'))
-                .click()
-            cy.get(Cypress.config('continueShopingBtn'))
-                .click()
+            it('Should search for a product then click on the first result', () => {
+                cy.get(Cypress.config('searchBar')).type('iphone {enter}')
+                cy.wait(1000)
+                cy.get(Cypress.config('firstProduct')).click()
+            })
 
-            cy.get(Cypress.config('cartBtn'))
-                .click()
-            cy.get(Cypress.config('productNameFromCartPage'))
-                .should('contain', `${productName}`)
-        })
+            it("Should save the product's name", () => {
+                cy.get(Cypress.config('productNmaeFromProductPage'))
+                    .then(message => {
+                        productName = message.text()
 
-        it('Should remove the product from cart', () => {
-            cy.get(Cypress.config('removeBtn'))
-                .click()
+                    })
+            })
 
-            cy.get(Cypress.config('cartBtn'))
-                .click()
-            cy.get(Cypress.config('mainBodySecstion'))
-                .should('contain', 'Your shopping cart looks empty')
-        })
-    })
+            it('Should add the product to cart', () => {
+                cy.get(Cypress.config('addToCartBtn'))
+                    .click()
+                cy.get(Cypress.config('continueShopingBtn'))
+                    .click()
 
-    context('IPhone-X Resolution (375 x 812)', () => {
-        var productName = ''
+                cy.get(Cypress.config('cartBtn'))
+                    .click()
+                cy.get(Cypress.config('productNameFromCartPage'))
+                    .should('contain', `${productName}`)
+            })
 
-        beforeEach(() => {
-            cy.viewport('iphone-x')
-        })
+            it('Should remove the product from cart', () => {
+                cy.get(Cypress.config('removeBtn'))
+                    .click()
 
-        it('Should visit the home page', () => {
-            cy.visit(Cypress.config('baseUrl'))
-        })
-
-        it('Should search for a product then click on the first result', () => {
-            cy.get(Cypress.config('searchBar')).type('iphone {enter}')
-            cy.wait(1000)
-            cy.get(Cypress.config('firstProduct')).click()
-        })
-
-        it("Should save the product's name", () => {
-            cy.get(Cypress.config('productNmaeFromProductPage'))
-                .then(message => {
-                    productName = message.text()
-
-                })
-        })
-
-        it('Should add the product to cart', () => {
-            cy.get(Cypress.config('addToCartBtn'))
-                .click()
-            cy.get(Cypress.config('continueShopingBtn'))
-                .click()
-
-            cy.get(Cypress.config('cartBtn'))
-                .click()
-            cy.get(Cypress.config('productNameFromCartPage'))
-                .should('contain', `${productName}`)
-        })
-
-        it('Should remove the product from cart', () => {
-            cy.get(Cypress.config('removeBtn'))
-                .click()
-
-            cy.get(Cypress.config('cartBtn'))
-                .click()
-            cy.get(Cypress.config('mainBodySecstion'))
-                .should('contain', 'Your shopping cart looks empty')
+                cy.get(Cypress.config('cartBtn'))
+                    .click()
+                cy.get(Cypress.config('mainBodySecstion'))
+                    .should('contain', 'Your shopping cart looks empty')
+            })
         })
     })
 })
